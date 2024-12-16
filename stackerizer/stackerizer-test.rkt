01259e7e02c25938a37caabdd16e9690d9f5e038
Joe <86804949+prototyperesolution@users.noreply.github.com>
2024-12-16 10:07:18 +0100

    final stacker

---
+++ stackerizer/stackerizer-test.rkt
@@ -0,0 +1,3 @@
+#lang s-exp "stackerizer.rkt"
+
+(* 18 (+ 10 100 3400))
---
+++ stackerizer/stackerizer.rkt
@@ -0,0 +1,26 @@
+#lang br/quicklang
+(provide + *)
+
+(define-macro (stackerizer-mb EXPR)
+  #'(#%module-begin
+    (for-each displayln (reverse (flatten EXPR)))))
+(provide (rename-out [stackerizer-mb #%module-begin]))
+
+(define-macro (define-op OP)
+    #'(define-macro-cases OP
+        [(OP FIRST) #'FIRST]
+        [(OP FIRST NEXT (... ...)) #'(list 'OP FIRST (+ NEXT (... ...)))]
+    )
+)
+
+(define-macro (define-ops OP ...)
+    #'(begin                                            ;
+    (define-macro-cases OP                              ;
+        [(OP FIRST) #'FIRST]                            ;
+        [(OP FIRST NEXT (... ...))                      ;
+            #'(list 'OP FIRST (OP NEXT (... ...)))])    ; (... ...) is because we want ellipses in the inner macro, not the outer
+        ...                                             ; The ... operator uses preceding arg as a template
+    )
+)
+
+(define-ops + *)
